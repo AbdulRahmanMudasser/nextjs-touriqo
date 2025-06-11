1bcc9baa6b59890882caa3460b22e2770218ea31
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-06-11 14:33:21 +0500

    fix: resolved errors in api.service.ts
    - Unexpected any. Specify a different type.

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -28,11 +28,11 @@ axiosInstance.interceptors.response.use(
 export const apiService = {
   get: <T>(url: string, config: AxiosRequestConfig = {}) => 
     axiosInstance.get<T>(url, config),
-  post: <T>(url: string, data: any, config: AxiosRequestConfig = {}) => 
+  post: <T, D>(url: string, data: D, config: AxiosRequestConfig = {}) => 
     axiosInstance.post<T>(url, data, config),
-  put: <T>(url: string, data: any, config: AxiosRequestConfig = {}) => 
+  put: <T, D>(url: string, data: D, config: AxiosRequestConfig = {}) => 
     axiosInstance.put<T>(url, data, config),
-  patch: <T>(url: string, data: any, config: AxiosRequestConfig = {}) => 
+  patch: <T, D>(url: string, data: D, config: AxiosRequestConfig = {}) => 
     axiosInstance.patch<T>(url, data, config),
   delete: <T>(url: string, config: AxiosRequestConfig = {}) => 
     axiosInstance.delete<T>(url, config),
