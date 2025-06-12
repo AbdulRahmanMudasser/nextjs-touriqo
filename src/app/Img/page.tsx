46514922e181435cd60b17a663fbc8d0d0aeb9e6
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-06-12 16:35:59 +0500

    refactor: uncommented Img/page.tsx

--- src/app/Img/page.tsx
+++ src/app/Img/page.tsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import Img from '../Components/img';
+import React from 'react';
+import Img from '../Components/img';
 // import Header from '../Components/header';
-// import TourInformation from '../Components/tourInformation';
+import TourInformation from '../Components/tourInformation';
 // import TourBooking from '../Components/tourBooking';
-// import TourPlan from '../Components/tourplan';
-// import Map from '../Components/map';
-// import Calender from '../Components/calender';
-// import ReviewScores from '../Components/reviewscore';
-// import TourComments from '../Components/tourComment';
+import TourPlan from '../Components/tourplan';
+import Map from '../Components/map';
+import Calender from '../Components/calender';
+import ReviewScores from '../Components/reviewscore';
+import TourComments from '../Components/tourComment';
 
-// export default function Page() {
-//   return (
-//     <div className="font-sans">
+export default function Page() {
+  return (
+    <div className="font-sans">
       
-//       <Img />
-//       <Header />
+      <Img />
+      {/* <Header /> */}
   
-//       <TourInformation />
-//       <TourBooking />
-//       <TourPlan />
-//       <Map />
-//     <Calender />
-//       <ReviewScores />
-//       <TourComments />
-//     </div>
-//   );
-// }
+      <TourInformation />
+      {/* <TourBooking /> */}
+      <TourPlan />
+      <Map />
+    <Calender />
+      <ReviewScores />
+      <TourComments />
+    </div>
+  );
+}
